a5c1a2b4b233ea1b1d132b4b3634b30aa001287d
Bmborba87 <brunogiovaniborba@gmail.com>
2026-07-31 18:32:34 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="betmanager-v39";
+const CACHE="betmanager-v40";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));

a8b1682cc38c50ac6660a8459257f4f85cc2076d
Bmborba87 <brunogiovaniborba@gmail.com>
2026-07-31 15:05:48 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="controle-apostas-v19";
+const CACHE="controle-apostas-v20";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
